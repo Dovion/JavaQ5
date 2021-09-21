dd14bfb9fe290f12d372d78ad09ab22cc024a452
Greds <vyacheslav-98@yandex.ru>
2021-09-21 18:34:47 +0300

    Format updated

--- src/com/company/BonusMilesService.java
+++ src/com/company/BonusMilesService.java
@@ -1,9 +1,7 @@
 package com.company;
 
 public class BonusMilesService {
-    public int calculate (int cost){
-        var result = cost/20;
-        return result;
-
+    public int calculate(int cost) {
+        return cost / 20;
     }
 }
